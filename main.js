b273e096c08592ad91993074fb1306c4842be188
heisir <heisir21@163.com>
2021-01-17 00:36:42 +0800

    update serverURL

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ let logger;
         logger.info('loadExtension success');
 
         let param = {
-            serverURL: '',
+            serverURL: Buffer.from('aHR0cHM6Ly96aGliby5kamRldmVsb3Blci5jbjozMDA0Lw==','base64').toString(),
             room:'8888',
             userId:'any',
             userNick:'游客'
